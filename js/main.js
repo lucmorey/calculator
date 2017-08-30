547f9ad3048824f3a2fb8b651d92713b21973ef1
Luc Morey <lucmorey@gmail.com>
2017-08-29 21:59:42 -0700

    js fixed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,56 @@
-console.log('here')
-
 var readout = document.querySelector('#readout')
-var digits = document.querySelectorAll('.digit')
+var digits = document.querySelectorAll('digit')
+var operators = document.querySelectorAll(".operators")
+var equalsBtn = document.querySelector("#equals")
+var clearBtn = document.querySelector("#clear")
+var firstNumber = ''
+var secondNumber = ''
+var operator = ''
+function evaluate (operation, firstNumber, secondNumber) {
+switch(operation) {
+    case '+':
+        return firstNumber + secondNumber
+    case '-':
+        return firstNumber - secondNumber
+    case '*':
+        return firstNumber * secondNumber
+    case '/':
+        return firstNumber / secondNumber
+    }
+}
 
 // for each digit, do a function
 digits.forEach(function(btn) {
-    // add an event listener to each button
-    btn.addEventListener('click',function() {
+	// add an event listener to each button
+	btn.addEventListener('click', function() {
         // 'this' is the button being clicked...
-        console.log(this.innerText)
-        readout.innerText += this.innerText
-    })
+        readout.innerText+=this.innerText
+
+	})
 })
 
-var operators = document.querySelectorAll(".operators")
+// a separate forEach for the operators
 operators.forEach(function(btn) {
-    btn.addEventListener('click',function() {
-        readout.innerText += this.innerText
-    })
+	btn.addEventListener('click', function() {
+        if(firstNumber) {
+		secondNumber = Number(readout.innerText)
+		readout.innerText = evaluate(operator,firstNumber,secondNumber)
+		// change this to store variables that you will eventually use when you click =
+    }
+    firstNumber = Number(readout.innerText)
+    operator = this.innerText
+    readout.innerText =''
+})
+})
+
+equalsBtn.addEventListener('click', function() {
+	secondNumber = Number(readout.innerText)
+	readout.innerText = evaluate(operator, firstNumber, secondNumber)
+})
+clearBtn.addEventListener('click',function(){
+firstNumber = ''
+secondNumber = ''
+operation = ''
+readout.innerText = ''
+
 })
